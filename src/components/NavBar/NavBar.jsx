5cbf5f770836713f764692fca8cb7e7df42b054c
Israel S. Santos <israel.santos@amcom.com.br>
2022-03-24 02:51:34 -0300

    fix nav border bug

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { } from "./navBar.scss";
 
 export default function NavBar() {
+
+  let pathName = window.location.pathname;
+
+  useEffect(() => {
+    const curPage = document.getElementsByClassName(`${pathName}`)[0].classList.add('active');
+  }, [pathName])
 
   return (<>
     <div className="navbar">
@@ -20,16 +26,16 @@ export default function NavBar() {
         <nav>
           <ul>
             <li>
-              <Link to="/"><h4><span>00</span> HOME</h4></Link>
-            </li>
-            <li >
-              <Link to="/destination"><h4><span>01</span> DESTINATION</h4> </Link>
-            </li>
-            <li >
-              <Link to="/crew"><h4><span>02</span> CREW</h4></Link>
+              <Link to="/" className="/"><h4><span>00</span> HOME</h4></Link>
             </li>
             <li>
-              <Link to="/technology"><h4><span>03</span> TECHNOLOGY</h4></Link>
+              <Link to="/destination" className="/destination"><h4><span>01</span> DESTINATION</h4> </Link>
+            </li>
+            <li>
+              <Link to="/crew" className="/crew"><h4><span>02</span> CREW</h4></Link>
+            </li>
+            <li>
+              <Link to="/technology" className="/technology"><h4><span>03</span> TECHNOLOGY</h4></Link>
             </li>
           </ul>
         </nav>
