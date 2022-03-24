00d05b9640dfaf554d21fc5acb620e11b9e32196
Israel S. Santos <israel.santos@amcom.com.br>
2022-03-24 17:52:41 -0300

    fix nav click bug

--- src/components/NavPlanets/NavPlanets.jsx
+++ src/components/NavPlanets/NavPlanets.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { } from './navPlanets.scss'
 
 export default function NavPlanets() {
 
+
+  const [planetName, setPlanetName] = useState("");
+
+
   const setBorder = (e) => {
     let el = document.querySelectorAll('.activa');
+    let namePlanet = e.innerHTML;
+    setPlanetName(namePlanet);
+    console.log(namePlanet);
     el.forEach((elemento) => {
       elemento.classList.remove('activa');
     })
     e.classList.add('activa');
+
   }
   return (<div className="navPlanets">
     <nav>
-      <ul onClick={(e) => { setBorder(e.target) }}>
+      <ul onClick={(e) => {
+        if (e.target.nodeName === 'H5') {
+          setBorder(e.target)
+        } else {
+          e.preventDefault();
+        }
+
+      }}>
         <li><h5>MOOM</h5></li>
         <li><h5>MARS</h5></li>
         <li><h5>EUROPA</h5></li>
