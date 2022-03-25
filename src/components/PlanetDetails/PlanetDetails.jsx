a41a3339aff940c13371b25bd67a44482bdfc90a
Israel S. Santos <israel.santos@amcom.com.br>
2022-03-24 21:54:33 -0300

    fix home title h5

---
+++ src/components/PlanetDetails/PlanetDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { } from './planetDetails.scss'
+
+const PlanetDetail = (props) => {
+  return (<>
+
+  </>)
+}
+
+export default PlanetDetail;
--- src/screens/Destination/Destination.jsx
+++ src/screens/Destination/Destination.jsx
@@ -3,11 +3,13 @@ import NavBar from "../../components/NavBar/NavBar";
 import TitlePage from '../../components/TitlePage/TitlePage';
 import NavPlanets from '../../components/NavPlanets/NavPlanets';
 import { } from './destination.scss';
+import PlanetDetail from "../../components/PlanetDetails/PlanetDetails";
 
 export default function Destination() {
   return (<div className="destination">
     <NavBar />
     <TitlePage numberTitle={"01"} title={"PICK YOUR DESTINATION"} />
     <NavPlanets />
+    <PlanetDetail />
   </div>)
 }
