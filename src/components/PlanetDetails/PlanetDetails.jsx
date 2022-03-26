507e778777af8ca6c5186391689415b932f1e6ff
Israel S. Santos <israel.santos@amcom.com.br>
2022-03-25 21:03:28 -0300

    fix images link on json file

--- src/components/PlanetDetails/PlanetDetails.jsx
+++ src/components/PlanetDetails/PlanetDetails.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import data from '../../data.json'
 import { } from './planetDetails.scss'
 
 const PlanetDetail = (props) => {
-  return (<>
 
-  </>)
+  const planetImagePic = data.destinations[0].images.png;
+  console.log(planetImagePic)
+
+  // console.log(data.destinations[0].name) 
+
+  return (<div className='planetDetailContainer'>
+    <div className='planetImageContainer'>
+      <div className='planetImage' style={{
+        backgroundImage: `url("${planetImagePic}")`
+      }} />
+    </div>
+    <div className='planetDetailDescriptionBox'>
+
+    </div>
+  </div>)
 }
 
-export default PlanetDetail;
+export default PlanetDetail; 
