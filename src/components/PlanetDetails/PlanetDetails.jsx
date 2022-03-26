8789c9d48be194e91bc3561d7f7cc480dad5a825
Israel S. Santos <israel.santos@amcom.com.br>
2022-03-25 22:18:38 -0300

    destination img and description and nav finish

--- src/components/PlanetDetails/PlanetDetails.jsx
+++ src/components/PlanetDetails/PlanetDetails.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import data from '../../data.json'
+import DefaultP from '../Text/DefaultP'
 import { } from './planetDetails.scss'
 
 const PlanetDetail = (props) => {
 
   const planetImagePic = data.destinations[0].images.png;
-  console.log(planetImagePic)
+  const planetDescription = data.destinations[0].description;
 
   // console.log(data.destinations[0].name) 
 
   return (<div className='planetDetailContainer'>
     <div className='planetImageContainer'>
-      <div className='planetImage' style={{
-        backgroundImage: `url("${planetImagePic}")`
-      }} />
+      <div className='planetImage' style={{ backgroundImage: `url("${planetImagePic}")` }} />
     </div>
+
     <div className='planetDetailDescriptionBox'>
+      <div className='upSideDescription'>
+        <h2> MOON </h2>
+      </div>
+      <div className='downSideDescription'>
+        <DefaultP text={planetDescription} />
+      </div>
+
+      <div className='lineDivision' />
+
 
     </div>
   </div>)
